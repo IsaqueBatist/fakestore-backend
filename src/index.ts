6fe5ca6f6c9daba3ee20fc54b67eedf8ac86c2a2
Isaque Barbosa <isaquebatista400@gmail.com>
2025-07-10 17:06:00 -0300

    :wrench: chore: configure build with TypeScript and ts-node-dev

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { server } from './server/server';
 
-server.listen(3333, () => console.log('App rodando'));
+server.listen(process.env.PORT || 3333, () => console.log(`Porta rodando na porta ${process.env.PORT || 3333}`));
