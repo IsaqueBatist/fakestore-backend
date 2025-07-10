dfe8f1831c8c3f8c5a9010038be39712230800b1
Isaque Barbosa <isaquebatista400@gmail.com>
2025-07-10 19:01:14 -0300

    :bug: fix: resolves error TS2769 in ProductController.create

--- src/server/controllers/products/Create.ts
+++ src/server/controllers/products/Create.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 interface IProduct {
   name: string
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export const create = (req: Request<{}, {}, IProduct>, res: Response) => { 
-  console.log(req.body);
+export const create = (req: Request<{}, {}, IProduct>, res: Response) => {
+  if (req.body.name === undefined) {
+    res.status(StatusCodes.BAD_REQUEST).send("Informe o atributo nome")
+  }
   return res.send('Create');
 };
