3586c2f8d46828a049fa7ddf844b4c1ca447f3be
Isaque Barbosa <isaquebatista400@gmail.com>
2025-07-17 20:51:11 -0300

    :bug: fix CORS error

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,8 +1,23 @@
 import express from 'express';
+import cors from 'cors'
+import dotenv from 'dotenv'
+import path from 'path'
+
 import { router } from './routes';
 import './shared/services'
 
-const server = express();
+
+// Carrega variáveis do .env da raiz do projeto
+dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') });
+
+const server   = express();
+
+server.use(cors({
+  origin:  process.env.ENABLED_CORS
+  ? process.env.ENABLED_CORS.split(';')
+  : '*'
+  //Adicionar endereço do frontend depois
+}));
 
 server.use(express.json());
 
